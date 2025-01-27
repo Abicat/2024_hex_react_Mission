03fac6910cb271234ecaa4547869d3f56bed0dfe
Abicat <79410104+Abicat@users.noreply.github.com>
2025-01-27 20:45:55 +0800

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   //編譯路徑
-  base:process.env.NODE_ENV === 'production' ? '/2024_hex_react_Mission/':'/',
+  base:process.env.NODE_ENV === 'production' ? '/2024_hex_react_Mission2/':'/',
   plugins: [react()],
 })
