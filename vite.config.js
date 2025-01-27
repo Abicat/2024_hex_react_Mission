9f8c4c32deac6420e19402816880ad683c518515
Abicat <79410104+Abicat@users.noreply.github.com>
2025-01-27 20:43:31 +0800

    gh-pages 設定

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //編譯路徑
+  base:process.env.NODE_ENV === 'production' ? '/2024_hex_react_Mission/':'/',
   plugins: [react()],
 })
